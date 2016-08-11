d8c6fbd3e1274614cf61a829f893ae6c46371dfd
Nataliia Solomina <nataliia.solomina@gmail.com>
2016-08-11 17:45:04 +0200

    f

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -37,7 +37,7 @@ class CocktailsController < ApplicationController
 
   def destroy
     @cocktail.destroy
-    redirect_to cocktail_path
+    redirect_to cocktails_path
 
   end
   # POST /cocktails
